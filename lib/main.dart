eb2c72400b33e0040f39bfa989f4516bcadc9c97
Kusal Damsara <kusal2002d@Gmail.com>
2025-12-23 23:05:26 +0530

    Remove unnecessary blank line in main function

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 void main() async {
-
   WidgetsFlutterBinding.ensureInitialized();
   await SharedPreferences.getInstance();
 
