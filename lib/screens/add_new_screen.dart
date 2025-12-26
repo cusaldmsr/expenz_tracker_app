1ff0fb1d922b7d83a1385bc9aa08e9beb0b689f8
Kusal Damsara <kusal2002d@Gmail.com>
2025-12-26 23:11:34 +0530

    Refactor AddNewScreen layout: enhance input field for amount and add category selector dropdown

--- lib/screens/add_new_screen.dart
+++ lib/screens/add_new_screen.dart
@@ -1,5 +1,7 @@
 import 'package:expenz_tracker_app/constants/colors.dart';
 import 'package:expenz_tracker_app/constants/constants.dart';
+import 'package:expenz_tracker_app/models/expens_model.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 class AddNewScreen extends StatefulWidget {
@@ -18,12 +20,11 @@ class _AddNewScreenState extends State<AddNewScreen> {
       backgroundColor: _selectedMethod == 0 ? kRed : kGreen,
       body: SafeArea(
         child: SingleChildScrollView(
-          padding: const EdgeInsets.symmetric(horizontal: kDefaultPadding),
-          child: Padding(
-            padding: const EdgeInsets.symmetric(vertical: kDefaultPadding),
-            child: Stack(
-              children: [
-                Container(
+          child: Stack(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(kDefaultPadding),
+                child: Container(
                   height: MediaQuery.of(context).size.height * 0.06,
                   decoration: BoxDecoration(
                     color: kWhite,
@@ -87,46 +88,83 @@ class _AddNewScreenState extends State<AddNewScreen> {
                     ],
                   ),
                 ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(
-                    horizontal: kDefaultPadding,
-                  ),
-                  child: Container(
-                    margin: const EdgeInsets.only(top: kDefaultPadding * 6),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          'How Much?',
-                          style: TextStyle(
-                            color: kLightGrey.withOpacity(0.8),
-                            fontSize: 18,
-                            fontWeight: FontWeight.w500,
-                          ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: kDefaultPadding,
+                ),
+                child: Container(
+                  margin: const EdgeInsets.only(top: kDefaultPadding * 6),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'How Much?',
+                        style: TextStyle(
+                          color: kLightGrey.withOpacity(0.8),
+                          fontSize: 18,
+                          fontWeight: FontWeight.w500,
                         ),
-                        TextField(
-                          keyboardType: TextInputType.number,
-                          style: const TextStyle(
+                      ),
+                      TextField(
+                        keyboardType: TextInputType.number,
+                        style: const TextStyle(
+                          fontSize: 60,
+                          fontWeight: FontWeight.bold,
+                          color: kWhite,
+                        ),
+                        decoration: const InputDecoration(
+                          border: InputBorder.none,
+                          hintText: '0',
+                          hintStyle: TextStyle(
                             fontSize: 60,
                             fontWeight: FontWeight.bold,
                             color: kWhite,
                           ),
-                          decoration: const InputDecoration(
-                            border: InputBorder.none,
-                            hintText: '0',
-                            hintStyle: TextStyle(
-                              fontSize: 60,
-                              fontWeight: FontWeight.bold,
-                              color: kWhite,
-                            ),
-                          ),
                         ),
-                      ],
-                    ),
+                      ),
+                    ],
                   ),
                 ),
-              ],
-            ),
+              ),
+
+              //user data form
+              Container(
+                margin: EdgeInsets.only(
+                  top: MediaQuery.of(context).size.height * 0.35,
+                ),
+                padding: const EdgeInsets.all(kDefaultPadding),
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  color: kWhite,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(30),
+                    topRight: Radius.circular(30),
+                  ),
+                ),
+                child: Form(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      //Category selector dropdown
+                      DropdownButtonFormField(
+                        decoration: const InputDecoration(
+                          labelText: 'Select Category',
+                          border: InputBorder.none,
+                        ),
+                        items: ExpensCategory.values.map((category) {
+                          return DropdownMenuItem(
+                            value: category,
+                            child: Text(describeEnum(category)),
+                          );
+                        }).toList(),
+                        onChanged: (value) {},
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
           ),
         ),
       ),
