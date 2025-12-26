e98d695e62d016bd4f7eb3aa96c39a1591f84fb7
Kusal Damsara <kusal2002d@Gmail.com>
2025-12-26 18:08:48 +0530

    Implement bottom navigation bar in MainScreen for improved navigation

--- lib/screens/onboarding/main_screen.dart
+++ lib/screens/onboarding/main_screen.dart
@@ -1,3 +1,4 @@
+import 'package:expenz_tracker_app/constants/colors.dart';
 import 'package:flutter/material.dart';
 
 class MainScreen extends StatefulWidget {
@@ -11,7 +12,37 @@ class _MainScreenState extends State<MainScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Onboarding Main Screen')),
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        selectedItemColor: kMainColor,
+        unselectedItemColor: kGrey,
+        showUnselectedLabels: true,
+        backgroundColor: kWhite,
+        selectedLabelStyle: const TextStyle(
+          fontSize: 12,
+          fontWeight: FontWeight.w600,
+        ),
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.list_rounded),
+            label: 'Transactions',
+          ),
+          BottomNavigationBarItem(
+            icon: Container(
+              padding: EdgeInsets.all(8.0),
+              decoration: BoxDecoration(
+                color: kMainColor,
+                shape: BoxShape.circle,
+              ),
+              child: const Icon(Icons.add, color: kWhite, size: 30),
+            ),
+            label: '',
+          ),
+          BottomNavigationBarItem(icon: Icon(Icons.rocket), label: 'Budget'),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile'),
+        ],
+      ),
     );
   }
 }
