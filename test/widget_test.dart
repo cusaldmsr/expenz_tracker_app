d868d4af2a9165fd11d3eae494941b7f4233df3f
Kusal Damsara <kusal2002d@Gmail.com>
2025-12-26 16:46:31 +0530

    Add wait for FutureBuilder completion in widget test for reliable app loading

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -6,6 +6,9 @@ void main() {
   testWidgets('App loads successfully', (WidgetTester tester) async {
     await tester.pumpWidget(const MyApp());
 
+    // Wait for FutureBuilder to complete
+    await tester.pumpAndSettle();
+
     // Verify MaterialApp is present
     expect(find.byType(MaterialApp), findsOneWidget);
   });
