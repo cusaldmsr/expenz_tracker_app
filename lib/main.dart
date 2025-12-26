98d667e016d3f4daacd4ad580b5fb328ae0eee70
Kusal Damsara <kusal2002d@Gmail.com>
2025-12-26 13:13:25 +0530

    Implement FutureBuilder to check fullname storage before rendering the app

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:expenz_tracker_app/screens/onboarding_screen.dart';
+import 'package:expenz_tracker_app/services/user_services.dart';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
@@ -14,11 +15,25 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Expense Tracker',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(fontFamily: "Inter"),
-      home: const OnboardingScreen(),
-    );
+    return FutureBuilder(
+      future: UserService.checkFullname(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const CircularProgressIndicator();
+        } else {
+          final bool isFullnameStored = snapshot.data ?? false;
+          return MaterialApp(
+            debugShowCheckedModeBanner: false,
+            title: 'Expenz Tracker',
+            theme: ThemeData(
+              primarySwatch: Colors.blue,
+            ),
+            home: isFullnameStored
+                ? const OnboardingScreen()
+                : const OnboardingScreen(),
+          );
+        }
+      },
+      );
   }
 }
