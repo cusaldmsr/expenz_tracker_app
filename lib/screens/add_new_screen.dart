56763f2a240ed547a4448d1c833575a938bf2f5d
Kusal Damsara <kusal2002d@Gmail.com>
2026-01-02 10:48:44 +0530

    Enhance AddNewScreen: implement expense saving functionality with shared preferences and callback

--- lib/screens/add_new_screen.dart
+++ lib/screens/add_new_screen.dart
@@ -2,13 +2,16 @@ import 'package:expenz_tracker_app/constants/colors.dart';
 import 'package:expenz_tracker_app/constants/constants.dart';
 import 'package:expenz_tracker_app/models/expens_model.dart';
 import 'package:expenz_tracker_app/models/income_model.dart';
+import 'package:expenz_tracker_app/services/expense_services.dart';
 import 'package:expenz_tracker_app/widgets/custom_button.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
 class AddNewScreen extends StatefulWidget {
-  const AddNewScreen({super.key});
+
+  final Function(ExpensModel) onAddExpense;
+  const AddNewScreen({super.key, required this.onAddExpense});
 
   @override
   State<AddNewScreen> createState() => _AddNewScreenState();
@@ -359,8 +362,23 @@ class _AddNewScreenState extends State<AddNewScreen> {
                           width: double.infinity,
                           height: 50,
                           child: GestureDetector(
-                            onTap: () {
-                              //save the expense/income data
+                            onTap: () async{
+                              //save the expense/income data into shared preferences
+                              List<ExpensModel> existingExpenses = await ExpenseServices().fetchExpenses();
+                              
+                              //create a new expense to add
+                              ExpensModel newExpense = ExpensModel(
+                                id: existingExpenses.isNotEmpty ? existingExpenses.last.id + 1 : 1,
+                                title: _titleController.text,
+                                category: _expensCategory,
+                                amount: _amountController.text.isEmpty ? 0.0 : double.parse(_amountController.text),
+                                date: _selectedDate,
+                                time: _selectedTime,
+                                description: _descriptionController.text,
+                              );
+
+                              //call the onAddExpense callback to add the new expense
+                              widget.onAddExpense(newExpense);
                             },
                             child: CustomButton(
                               text: 'Add Now',
