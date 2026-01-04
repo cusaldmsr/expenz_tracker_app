03866108b722a61c86fb9f743ab5ea6076392211
Kusal Damsara <kusal2002d@Gmail.com>
2026-01-04 12:48:58 +0530

    Add IncomeCard widget to display income details with formatted amount and date

---
+++ lib/widgets/income_card.dart
@@ -0,0 +1,106 @@
+import 'package:expenz_tracker_app/constants/colors.dart';
+import 'package:expenz_tracker_app/models/income_model.dart';
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+class IncomeCard extends StatelessWidget {
+  final String title;
+  final DateTime date;
+  final double amount;
+  final IncomeCategory category;
+  final String description;
+  final DateTime createdAt;
+
+  const IncomeCard({
+    super.key,
+    required this.title,
+    required this.date,
+    required this.amount,
+    required this.category,
+    required this.description,
+    required this.createdAt,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.only(bottom: 20.0),
+      padding: const EdgeInsets.all(20.0),
+      decoration: BoxDecoration(
+        color: kWhite,
+        borderRadius: BorderRadius.circular(10.0),
+        boxShadow: [
+          BoxShadow(
+            color: kGrey.withOpacity(0.4),
+            spreadRadius: 1,
+            blurRadius: 10,
+            offset: const Offset(0, 1),
+          ),
+        ],
+      ),
+      child: Row(
+        children: [
+          Container(
+            height: 50,
+            width: 50,
+            decoration: BoxDecoration(
+              color: incomeCategoryColors[category]!.withOpacity(0.1),
+              borderRadius: BorderRadius.circular(10.0),
+            ),
+            child: Center(
+              child: Image.asset(
+                incomeCategoryImages[category]!,
+                height: 24,
+                width: 24,
+                color: incomeCategoryColors[category],
+              ),
+            ),
+          ),
+          const SizedBox(width: 20.0),
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  title,
+                  style: const TextStyle(
+                    fontSize: 16.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const SizedBox(height: 5.0),
+                Text(
+                  description,
+                  style: const TextStyle(
+                    fontSize: 14.0,
+                    color: kGrey,
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          SizedBox(width: 20.0),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              Text(
+                '+ ' + '\$${amount.toStringAsFixed(2)}',
+                style: const TextStyle(
+                  fontSize: 16.0,
+                  fontWeight: FontWeight.bold,
+                  color: kGreen,
+                ),
+              ),
+              const SizedBox(height: 5.0),
+              Text(
+                DateFormat.jm().format(createdAt),
+                style: const TextStyle(fontSize: 12.0, color: kGrey),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
