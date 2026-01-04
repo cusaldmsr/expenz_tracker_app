b1c2de8cbddff8a54b8d99e1b081340d014fdd10
Kusal Damsara <kusal2002d@Gmail.com>
2026-01-04 13:35:41 +0530

    Fix string formatting for amount display in ExpenseCard

--- lib/widgets/expense_card.dart
+++ lib/widgets/expense_card.dart
@@ -85,7 +85,7 @@ class ExpenseCard extends StatelessWidget {
             crossAxisAlignment: CrossAxisAlignment.end,
             children: [
               Text(
-                '- ' + '\$${amount.toStringAsFixed(2)}',
+                '- \$${amount.toStringAsFixed(2)}',
                 style: const TextStyle(
                   fontSize: 16.0,
                   fontWeight: FontWeight.bold,
