a0970eddb37865196e86d9fef71683bd3e03d158
Kusal Damsara <kusal2002d@Gmail.com>
2026-01-05 15:17:32 +0530

    Implement budget selection toggle for income and expense in BudgetScreen

--- lib/screens/budget_screen.dart
+++ lib/screens/budget_screen.dart
@@ -1,3 +1,6 @@
+import 'package:expenz_tracker_app/constants/colors.dart';
+import 'package:expenz_tracker_app/constants/constants.dart';
+import 'package:expenz_tracker_app/widgets/custom_button.dart';
 import 'package:flutter/material.dart';
 
 class BudgetScreen extends StatefulWidget {
@@ -8,8 +11,116 @@ class BudgetScreen extends StatefulWidget {
 }
 
 class _BudgetScreenState extends State<BudgetScreen> {
+  int _selectedMethod = 0; //0 for income, 1 for expense
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(title: const Text('Budget Screen')));
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Financial Report',
+          style: TextStyle(
+            color: kBlack,
+            fontWeight: FontWeight.bold,
+            fontSize: 20,
+          ),
+        ),
+        centerTitle: true,
+      ),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Stack(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(kDefaultPadding),
+                child: Container(
+                  height: MediaQuery.of(context).size.height * 0.06,
+                  decoration: BoxDecoration(
+                    color: kWhite,
+                    borderRadius: BorderRadius.circular(100),
+                    boxShadow: [
+                      BoxShadow(
+                        color: kGrey.withOpacity(0.4),
+                        spreadRadius: 1,
+                        blurRadius: 20,
+                        offset: const Offset(0, 1),
+                      ),
+                    ],
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      GestureDetector(
+                        onTap: () {
+                          setState(() {
+                            _selectedMethod = 0;
+                          });
+                        },
+                        child: Container(
+                          decoration: BoxDecoration(
+                            color: _selectedMethod == 0 ? kMainColor : kWhite,
+                            borderRadius: BorderRadius.circular(100),
+                          ),
+                          child: Padding(
+                            padding: const EdgeInsets.symmetric(
+                              horizontal: 66.0,
+                              vertical: 14.0,
+                            ),
+                            child: Text(
+                              'Income',
+                              style: TextStyle(
+                                color: _selectedMethod == 0 ? kWhite : kBlack,
+                                fontWeight: FontWeight.w600,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                      GestureDetector(
+                        onTap: () {
+                          setState(() {
+                            _selectedMethod = 1;
+                          });
+                        },
+                        child: Container(
+                          decoration: BoxDecoration(
+                            color: _selectedMethod == 1 ? kMainColor : kWhite,
+                            borderRadius: BorderRadius.circular(100),
+                          ),
+                          child: Padding(
+                            padding: const EdgeInsets.symmetric(
+                              horizontal: 66.0,
+                              vertical: 14.0,
+                            ),
+                            child: Text(
+                              'Expense',
+                              style: TextStyle(
+                                color: _selectedMethod == 1 ? kWhite : kBlack,
+                                fontWeight: FontWeight.w600,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: kDefaultPadding,
+                ),
+                child: Container(
+                  margin: const EdgeInsets.only(top: kDefaultPadding * 6),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
