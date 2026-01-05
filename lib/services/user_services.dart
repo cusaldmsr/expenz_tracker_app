957a1879ec23e08d8e4a5b3b162f8c5db38ef586
Kusal Damsara <kusal2002d@Gmail.com>
2026-01-05 21:55:40 +0530

    Refactor clearAllUserDetails method for consistent formatting and readability

--- lib/services/user_services.dart
+++ lib/services/user_services.dart
@@ -64,10 +64,9 @@ class UserService {
   //remove all user details from shared preferences
   static Future<void> clearAllUserDetails() async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
-    await prefs.remove( 'fullname');
+    await prefs.remove('fullname');
     await prefs.remove('email');
     await prefs.remove('phoneNumber');
     await prefs.remove('password');
   }
-
 }
