a47252cd0569ed718386bea85986dee431d517a7
Kusal Damsara <kusal2002d@Gmail.com>
2026-01-05 16:01:42 +0530

    Update BudgetPieChart to adjust expense category values and add income category sections

--- lib/widgets/budget_pie_chart.dart
+++ lib/widgets/budget_pie_chart.dart
@@ -1,3 +1,4 @@
+import 'package:expenz_tracker_app/constants/colors.dart';
 import 'package:expenz_tracker_app/models/expens_model.dart';
 import 'package:expenz_tracker_app/models/income_model.dart';
 import 'package:fl_chart/fl_chart.dart';
@@ -7,30 +8,24 @@ class BudgetPieChart extends StatefulWidget {
   final Map<ExpensCategory, double> expenseCategoryTotals;
   final Map<IncomeCategory, double> incomeCategoryTotals;
   final bool isExpense;
+
   const BudgetPieChart({
-    super.key,
+    Key? key,
     required this.expenseCategoryTotals,
     required this.incomeCategoryTotals,
     required this.isExpense,
-  });
+  }) : super(key: key);
 
   @override
   State<BudgetPieChart> createState() => _BudgetPieChartState();
 }
 
 class _BudgetPieChartState extends State<BudgetPieChart> {
-  //sections data for pie chart
   List<PieChartSectionData> getSections() {
     if (widget.isExpense) {
       return [
         PieChartSectionData(
           color: expensCategoryColors[ExpensCategory.food],
-          value: widget.expenseCategoryTotals[ExpensCategory.food] ?? 0,
-          showTitle: false,
-          radius: 60,
-        ),
-        PieChartSectionData(
-          color: expensCategoryColors[ExpensCategory.health],
           value: widget.expenseCategoryTotals[ExpensCategory.health] ?? 0,
           showTitle: false,
           radius: 60,
@@ -42,6 +37,12 @@ class _BudgetPieChartState extends State<BudgetPieChart> {
           radius: 60,
         ),
         PieChartSectionData(
+          color: expensCategoryColors[ExpensCategory.transport],
+          value: widget.expenseCategoryTotals[ExpensCategory.transport] ?? 0,
+          showTitle: false,
+          radius: 60,
+        ),
+        PieChartSectionData(
           color: expensCategoryColors[ExpensCategory.subscriptions],
           value:
               widget.expenseCategoryTotals[ExpensCategory.subscriptions] ?? 0,
@@ -49,18 +50,76 @@ class _BudgetPieChartState extends State<BudgetPieChart> {
           radius: 60,
         ),
         PieChartSectionData(
-          color: expensCategoryColors[ExpensCategory.transport],
-          value: widget.expenseCategoryTotals[ExpensCategory.transport] ?? 0,
+          color: expensCategoryColors[ExpensCategory.food],
+          value: widget.expenseCategoryTotals[ExpensCategory.food] ?? 0,
+          showTitle: false,
+          radius: 60,
+        ),
+      ];
+    } else {
+      return [
+        PieChartSectionData(
+          color: incomeCategoryColors[IncomeCategory.salary],
+          value: widget.incomeCategoryTotals[IncomeCategory.salary] ?? 0,
+          showTitle: false,
+          radius: 60,
+        ),
+        PieChartSectionData(
+          color: incomeCategoryColors[IncomeCategory.freelance],
+          value: widget.incomeCategoryTotals[IncomeCategory.freelance] ?? 0,
+          showTitle: false,
+          radius: 60,
+        ),
+        PieChartSectionData(
+          color: incomeCategoryColors[IncomeCategory.passiveIncome],
+          value: widget.incomeCategoryTotals[IncomeCategory.passiveIncome] ?? 0,
+          showTitle: false,
+          radius: 60,
+        ),
+        PieChartSectionData(
+          color: incomeCategoryColors[IncomeCategory.freelance],
+          value: widget.incomeCategoryTotals[IncomeCategory.sales] ?? 0,
           showTitle: false,
           radius: 60,
         ),
       ];
     }
-    return [];
   }
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    final PieChartData pieChartData = PieChartData(
+      sectionsSpace: 0,
+      centerSpaceRadius: 70,
+      startDegreeOffset: -90,
+      sections: getSections(),
+      borderData: FlBorderData(show: false),
+    );
+
+    return Container(
+      height: 250,
+      padding: const EdgeInsets.all(16),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(16),
+      ),
+      child: Stack(
+        alignment: Alignment.center,
+        children: [
+          PieChart(pieChartData),
+          const Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                "70%",
+                style: TextStyle(color: kBlack, fontWeight: FontWeight.bold),
+              ),
+              SizedBox(height: 8),
+              Text("of 100%", style: TextStyle(color: Colors.grey)),
+            ],
+          ),
+        ],
+      ),
+    );
   }
 }
